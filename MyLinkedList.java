97b3aa925894427c0e26c8558014934c6e2c9020
Mohammad Jawwad <mjawwad@stuy.edu>
2018-12-12 19:26:00 -0500

    Added second constructor for Node: taking the reference (or lack of) of the previous node and the next node

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,9 +1,15 @@
 class Node {
   private int data;
   private Node next, prev;
-
+  //create a Node given a value
   public Node(int value) {
     data = value;
+  }
+  //create a Node that specifies the reference (or none) of the previous and future node
+  public Node(int value, Node nextNode, Node prevNode) {
+    data = value;
+    next = nextNode;
+    prev = prevNode;
   }
 }
 
