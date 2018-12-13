49dd6ec05159e43664f0e5c7c3505e82271c8c5a
Mohammad Jawwad <mjawwad@stuy.edu>
2018-12-12 19:23:08 -0500

    Added bodies so it could compile, added constructor for Node.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,13 +1,23 @@
 class Node {
   private int data;
   private Node next, prev;
+
+  public Node(int value) {
+    data = value;
+  }
 }
 
 public class MyLinkedList{
   private int size;
   private Node start,end;
 
-  public int size();
-  public boolean add(int value);
-  public String toString();
+  public int size() {
+    return size;
+  }
+  public boolean add(int value){
+    return false;
+  }
+  public String toString(){
+  return "empty now";
+  }
 }
