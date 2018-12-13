c560edd4a8e4c897006f64b21da52379e9237d0e
Mohammad Jawwad <mjawwad@stuy.edu>
2018-12-12 20:13:58 -0500

    Updated add after sanitychecks cuz OOO boi I hate thinking Nodes in Nodes. Also toString added.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,33 +1,37 @@
-class Node {
-  private int data;
-  private Node next, prev;
-  //create a Node given a value
-  public Node(int value) {
-    data = value;
-  }
-  //create a Node that specifies the reference (or none) of the previous and future node
-  public Node(int value, Node nextNode, Node prevNode) {
-    data = value;
-    next = nextNode;
-    prev = prevNode;
-  }
-  //Helping to see vals (we're gonna need this for toString too)
-  public int getData() {
-    return data;
-  }
-}
-
 public class MyLinkedList{
   private int size;
   private Node start,end;
 
+  public MyLinkedList() {
+    size = 0;
+  }
   public int size() {
     return size;
   }
   public boolean add(int value){
-    return false;
+    Node n = new Node(value, null, end);
+    if (end == null) {
+      end = n;
+      start = n;
+    }
+    else {
+      end.setNext(n);
+      end = n;
+    }
+    size++;
+    return true;
   }
   public String toString(){
-  return "empty now";
+    String out = "[";
+    if (start == null) return "[]";
+    Node curr = start;
+    while (curr.getNext() != null) {
+      //just so we can keep going through each node!
+      out+=curr + ", ";
+      curr = curr.getNext();
+      //we needda keep updating!
+    }
+    out+= curr + "]";
+    return out;
   }
 }
