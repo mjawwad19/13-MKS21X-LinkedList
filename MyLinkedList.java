98d374ea2e2252ba8c057a9fbe765bb53319a6c6
Mohammad Jawwad <mjawwad@stuy.edu>
2018-12-12 20:20:13 -0500

    Reorganized and added comment explanations

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,26 +1,17 @@
+import java.util.List;
+
 public class MyLinkedList{
   private int size;
   private Node start,end;
-
+  //everything needs a constructor if it's an object!
   public MyLinkedList() {
     size = 0;
   }
+  //basic fundamentals
   public int size() {
     return size;
   }
-  public boolean add(int value){
-    Node n = new Node(value, null, end);
-    if (end == null) {
-      end = n;
-      start = n;
-    }
-    else {
-      end.setNext(n);
-      end = n;
-    }
-    size++;
-    return true;
-  }
+  //toString just prints the contents of the linked list as if array containing each node's data val
   public String toString(){
     String out = "[";
     if (start == null) return "[]";
@@ -33,5 +24,22 @@ public class MyLinkedList{
     }
     out+= curr + "]";
     return out;
+  }
+  /* add a value to linked list
+  - if this is the end (or if the linked list is empty) then we have to change that!
+  - if this is the beginning we needa also make sure that it doesn't cut off the
+    other nodes!*/
+  public boolean add(int value){
+    Node n = new Node(value, null, end);
+    if (end == null) {
+      end = n;
+      start = n;
+    }
+    else {
+      end.setNext(n);
+      end = n;
+    }
+    size++;
+    return true;
   }
 }
